9e9c3076f65baf7dec33e70a7e9d663b6ea9fce8
Lijunmao <hackmong@163.com>
2021-10-11 15:52:39 +0800

    #64

--- server/base-server/internal/data/dao/train_job.go
+++ server/base-server/internal/data/dao/train_job.go
@@ -368,7 +368,7 @@ func (d *trainJobDao) GetTrainJobEvents(jobEventQuery *model.JobEventQuery) ([]*
 
 	objectName := fmt.Sprintf("%s-task%d-%d", jobEventQuery.Id, TaskIndex-1, ReplicaIndex-1)
 
-	countQuery := fmt.Sprintf("SELECT COUNT(%s) FROM events where object_name =~ /^%s/", keyMessage, objectName)
+	countQuery := fmt.Sprintf("SELECT COUNT(%s) FROM octopus..events where object_name = '%s'", keyMessage, objectName)
 	res, err := d.influxdb.Query(countQuery)
 
 	if err != nil {
@@ -380,7 +380,7 @@ func (d *trainJobDao) GetTrainJobEvents(jobEventQuery *model.JobEventQuery) ([]*
 		return events, 0, errors.Errorf(err, errors.ErroInfluxdbFindFailed)
 	}
 
-	query := fmt.Sprintf("select %s, %s, %s from events where object_name =~ /^%s/ and kind = 'Pod' LIMIT %d OFFSET %d", keyName, keyReason, keyMessage, objectName, PageSize, (PageIndex-1)*PageSize)
+	query := fmt.Sprintf("select %s, %s, %s from octopus..events where object_name = '%s' and kind = 'Pod' LIMIT %d OFFSET %d", keyName, keyReason, keyMessage, objectName, PageSize, (PageIndex-1)*PageSize)
 	res, err = d.influxdb.Query(query)
 
 	if err != nil {
