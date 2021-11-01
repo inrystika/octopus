99e9f4dd5e5fa69fae1debdf79c8b67da68dae77
linfengjun <linfj@pcl.ac.cn>
2021-11-01 14:27:39 +0800

    云际

--- server/base-server/internal/data/jointcloud/jointcloud_test.go
+++ server/base-server/internal/data/jointcloud/jointcloud_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func newJointCloud() JointCloud {
-	return NewJointCloud("http://192.168.207.141:8709", "test", "")
+	return NewJointCloud("http://192.168.207.141:8709", "test", "6c7c824d3b7342a9dcc9b931f5662b18")
 }
 func TestJointCloud_ListDataSet(t *testing.T) {
 	j := newJointCloud()
