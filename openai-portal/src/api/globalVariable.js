dd1d6a80d583e2a495955cd54ef1401380db39ba
wenlong92 <835952234@qq.com>
2021-11-10 14:54:24 +0800

    WIP:配置修改

--- openai-portal/src/api/globalVariable.js
+++ openai-portal/src/api/globalVariable.js
@@ -1,5 +1,5 @@
 let DOMAIN
-if (process.env.NODE_ENV === 'development') { DOMAIN = 'http://192.168.202.73' }
+if (process.env.NODE_ENV === 'development') { DOMAIN = 'http://192.168.203.156' }
 else { DOMAIN = window.location.protocol + '//' + document.domain }
 export default {
   DOMAIN
