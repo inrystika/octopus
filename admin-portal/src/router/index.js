4e042d49873344c4776df9c1c5d7cc907f4bc231
denglei <2522636547@qq.com>
2021-11-11 17:49:57 +0800

    数据集算法扩展

--- admin-portal/src/router/index.js
+++ admin-portal/src/router/index.js
@@ -104,12 +104,28 @@ export const constantRoutes = [
   {
     path: '/timeManager',
     component: Layout,
+    meta: {
+      title: '机时管理',
+      icon: 'time'
+    },
     children: [
       {
-        path: 'index',
-        name: '/',
-        component: () => import('@/views/timeManager/index'),
-        meta: { title: '机时管理', icon: 'time' }
+        path: 'machine',
+        name: 'machine',
+        component: () => import('@/views/timeManager/machine'),
+        meta: { title: '机时', icon: 'dot' }
+      },
+      {
+        path: 'recharge',
+        name: 'recharge',
+        component: () => import('@/views/timeManager/recharge'),
+        meta: { title: '充值', icon: 'dot' }
+      },
+      {
+        path: 'consumption',
+        name: 'consumption',
+        component: () => import('@/views/timeManager/consumption'),
+        meta: { title: '消费', icon: 'dot' }
       }
     ]
   },
