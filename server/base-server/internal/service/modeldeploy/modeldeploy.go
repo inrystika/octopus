7551be28c1e46a9c4870957df2391531196066e0
Liaoweisheng <liaowsh@pcl.ac.cn>
2021-11-30 17:32:49 +0800

    developing....

--- server/base-server/internal/service/modeldeploy/modeldeploy.go
+++ server/base-server/internal/service/modeldeploy/modeldeploy.go
@@ -2,14 +2,34 @@ package modeldeploy
 
 import (
 	"context"
+	"fmt"
 	"github.com/jinzhu/copier"
+	"google.golang.org/protobuf/types/known/emptypb"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientset "k8s.io/client-go/kubernetes"
 	api "server/base-server/api/v1"
+	"server/base-server/internal/common"
 	"server/base-server/internal/conf"
 	"server/base-server/internal/data"
 	"server/base-server/internal/data/dao/model"
 	"server/base-server/internal/data/pipeline"
+	"server/common/constant"
+	"server/common/errors"
 	"server/common/log"
 	"server/common/utils"
+	"time"
+	seldonv1 "github.com/seldonio/seldon-core/operator/apis/machinelearning.seldon.io/v1"
+	seldonv2 "github.com/seldonio/seldon-core/operator/apis/machinelearning.seldon.io/v1alpha2"
+)
+
+const (
+	deployJobKind   	= "deploy_job"
+	deployJobNum 		= 1
+	MetaNamePrefix      = "deploy-"
+	TensorFlowFrame     = "tensorflow"
+	PytorchFrame        = "pytorch"
 )
 
 type modelDeployService struct {
@@ -27,6 +47,18 @@ type modelDeployService struct {
 
 type ModelDeployService interface {
 	api.ModelDeployServiceServer
+}
+
+type startJobInfoSpec struct {
+	resources     map[v1.ResourceName]resource.Quantity
+	nodeSelectors map[string]string
+}
+
+type startJobInfo struct {
+	queue         string
+	modelFrame    string
+	modelPath     string
+	specs         map[string]*startJobInfoSpec
 }
 
 func NewModelDeployService(conf *conf.Bootstrap, logger log.Logger, data *data.Data,
@@ -68,12 +100,12 @@ func (s *modelDeployService) DeployModel(ctx context.Context, req *api.DepReques
 	}
 	deployJob.Id = modelServiceId
 	deployJob.Status = pipeline.PREPARING
-	//各类参数校验
+	//模型部署参数校验
 	startJobInfo, err := s.checkParam(ctx, deployJob)
 	if err != nil {
 		return nil, err
 	}
-	//submit job
+	//submit deploy job
 	closeFunc, err := s.submitDeployJob(ctx, deployJob, startJobInfo)
 	defer func() { //如果出错 重要的资源需要删除
 		if err != nil {
@@ -97,7 +129,7 @@ type deployInfo struct {
 
 type closeFunc func(ctx context.Context) error
 
-func (s *modelDeployService) submitDeployJob(ctx context.Context, modelDeploy *model.ModelDeploy, deployInfo *deployInfo) (closeFunc, error) {
+func (s *modelDeployService) submitDeployJob(ctx context.Context, modelDeploy *model.ModelDeploy, startJobInfo *startJobInfo) (closeFunc, error) {
 	var err error
 	closes := make([]closeFunc, 0)
 	resFunc := func(ctx context.Context) error {
@@ -118,21 +150,332 @@ func (s *modelDeployService) submitDeployJob(ctx context.Context, modelDeploy *m
 			_ = resFunc(ctx)
 		}
 	}()
-	return nil, nil
+
+	modelDeployName := modelDeploy.Name
+
+	param := &pipeline.SubmitJobParam{
+		UserID:       modelDeploy.UserId,
+		JobKind:      deployJobKind,
+		JobName:      modelDeploy.Id,
+		Header:       nil,
+		JobNamespace: modelDeploy.UserId,
+		//JobNamespace: "default",
+		Cluster: "",
+	}
+
+	minAvailable := 0
+
+	//挂载卷
+	volumeMounts := []v1.VolumeMount{
+		{
+			Name:      "modelFilePath",
+			MountPath: s.conf.Service.DockerModelDeployPath,
+			SubPath:   s.getModelSubPath(modelDeploy),
+			ReadOnly:  false,
+		},
+		{
+			Name:      "localtime",
+			MountPath: "/etc/localtime",
+		},
+	}
+
+	volumes := []v1.Volume{
+		{
+			Name: "modelFilePath",
+			VolumeSource: v1.VolumeSource{
+				PersistentVolumeClaim: &v1.PersistentVolumeClaimVolumeSource{
+					ClaimName: common.GetStoragePersistentVolumeChaim(modelDeploy.UserId),
+				},
+			},
+		},
+		{
+			Name: "localtime",
+			VolumeSource: v1.VolumeSource{
+				HostPath: &v1.HostPathVolumeSource{
+					Path: "/etc/localtime",
+				}},
+		},
+	}
+
+	//todo 修改模型路径
+	parameters := []seldonv1.Parameter{
+		{
+			Name: "model_volume_path",
+			Type: "STRING",
+			Value:"/app/models",
+
+		},
+		{
+			Name: "user_model_dir",
+			Type: "STRING",
+			//todo 用户模型目录
+			Value:"user2_model",
+		},
+	}
+
+	seldonPodSpecs := make([]*seldonv1.SeldonPodSpec,0)
+	seldonPodSpec := &seldonv1.SeldonPodSpec{
+		Spec: v1.PodSpec{
+			Containers:[]v1.Container{
+				{
+					Name:  modelDeployName,
+					VolumeMounts: volumeMounts,
+				},
+			},
+			Volumes: volumes,
+		},
+	}
+	seldonPodSpecs = append(seldonPodSpecs, seldonPodSpec)
+
+    var modelServer seldonv1.PredictiveUnitImplementation
+	if startJobInfo.modelFrame == TensorFlowFrame {
+		modelServer =  "TENSORFLOW_SERVER"
+	}else if startJobInfo.modelFrame == PytorchFrame{
+		modelServer =  "PYTORCH_SERVER"
+	}
+
+	graph := seldonv1.PredictiveUnit{
+		Name: modelDeployName,
+		Children: nil,
+		Implementation: &modelServer,
+		//todo 修改为pvc挂载
+		ModelURI: "gs:seldon-models/sklearn/iris",
+		Parameters: parameters,
+	}
+
+	var replica int32 = deployJobNum
+	predictors := make([]seldonv1.PredictorSpec,0)
+	predictor :=  seldonv1.PredictorSpec{
+		Name: "default",
+		Replicas: &replica,
+		ComponentSpecs: seldonPodSpecs,
+		Graph: graph,
+	}
+	predictors = append(predictors,predictor)
+
+	metadataName := fmt.Sprintf("%s%s ", MetaNamePrefix, modelDeploy.Name)
+	//pod template
+	modelSdep := &seldonv2.SeldonDeployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "machinelearning.seldon.io/v1alpha2",
+			Kind:       "SeldonDeployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: modelDeploy.UserId,
+			//Namespace: "default",
+			Name: modelDeploy.Id,
+		},
+		Spec: seldonv1.SeldonDeploymentSpec{
+			Predictors: predictors,
+		},
+	}
+	kubeConfig := s.data.Cluster.GetClusterConfig()
+	k8sClient := clientset.NewForConfigOrDie(kubeConfig)
+	_, podErr := k8sClient.CoreV1().S(modelDeploy.UserId).Create(context.TODO(), modelSdep, metav1.CreateOptions{})
+
+	if podErr != nil {
+
+	}
+	//if modelDeploy.Id != submitJobReply.JobId {
+	//	return nil, errors.Errorf(err, errors.ErrorPipelineDoRequest)
+	//}
+
+	return resFunc, nil
+
 }
 
-func (s *modelDeployService) checkParam(ctx context.Context, modelDeploy *model.ModelDeploy) (*deployInfo, error) {
-	return nil, nil
+func (s *modelDeployService) checkParam(ctx context.Context, modelDeploy *model.ModelDeploy) (*startJobInfo, error) {
+	//before commit job, check billing owner amount
+	ownerId, ownerType := s.getOwner(modelDeploy)
+	owner, err := s.billingService.GetBillingOwner(ctx, &api.GetBillingOwnerRequest{
+		OwnerId:   ownerId,
+		OwnerType: ownerType,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if owner.BillingOwner.Amount <= 0 {
+		return nil, errors.Errorf(nil, errors.ErrorTrainBalanceNotEnough)
+	}
+
+	queue := ""
+	if modelDeploy.WorkspaceId == constant.SYSTEM_WORKSPACE_DEFAULT {
+		pool, err := s.resourcePoolService.GetDefaultResourcePool(ctx, &emptypb.Empty{})
+		if err != nil {
+			return nil, err
+		}
+		queue = pool.ResourcePool.Name
+	} else {
+		workspace, err := s.workspaceService.GetWorkspace(ctx, &api.GetWorkspaceRequest{WorkspaceId: modelDeploy.WorkspaceId})
+		if err != nil {
+			return nil, err
+		}
+
+		queue = workspace.Workspace.ResourcePoolId
+	}
+	//check  model framework
+	modelFrameType := modelDeploy.ModelFrame
+    if  modelFrameType != TensorFlowFrame && modelFrameType != PytorchFrame{
+		return nil, errors.Errorf(err, errors.ErrorModelDeployForbidden)
+	}
+
+
+    //模型查询
+	model, err := s.getModelAndCheckPerm(ctx, modelDeploy.UserId, modelDeploy.WorkspaceId, modelDeploy.Id )
+	if err != nil {
+		return nil, err
+	}
+
+	//资源规格信息
+	startJobSpecs := map[string]*startJobInfoSpec{}
+	specs, err := s.resourceSpecService.ListResourceSpec(ctx, &api.ListResourceSpecRequest{})
+	if err != nil {
+		return nil, err
+	}
+	specMap := map[string]*api.ResourceSpec{}
+	for _, i := range specs.ResourceSpecs {
+		specMap[i.Id] = i
+	}
+	//资源
+	resourcesReply, err := s.resourceService.ListResource(ctx, &emptypb.Empty{})
+	if err != nil {
+		return nil, err
+	}
+	resourceMap := map[string]*api.Resource{}
+	for _, i := range resourcesReply.Resources {
+		resourceMap[i.Name] = i
+	}
+	//提交任务所需的资源规格映射表及节点标签映射表
+	resources := map[v1.ResourceName]resource.Quantity{}
+	nodeSelectors := map[string]string{}
+	//通过资源规格映射表，获取规格名称及价格
+	spec, ok := specMap[modelDeploy.ResourceSpecId]
+	if !ok {
+		return nil, errors.Errorf(err, errors.ErrorInvalidRequestParameter)
+	}
+	//解析资源规格包中的各项资源（cpu,gpu,memory等）的值
+	for k, v := range spec.ResourceQuantity {
+		//获取资源规格包中的key及value
+		r, ok := resourceMap[k]
+		if !ok {
+			return nil, errors.Errorf(err, errors.ErrorInvalidRequestParameter)
+		}
+		//解析资源规格value值
+		quantity, err := resource.ParseQuantity(v)
+		if err != nil {
+			return nil, err
+		}
+		if r.Name == k {
+			if r.ResourceRef == "" {
+				resources[v1.ResourceName(r.Name)] = quantity
+			} else {
+				resources[v1.ResourceName(r.ResourceRef)] = quantity
+				nodeSelectors[r.BindingNodeLabelKey] = r.BindingNodeLabelValue
+			}
+		}
+	}
+
+	startJobSpecs[modelDeploy.ResourceSpecId] = &startJobInfoSpec{
+		resources:     resources,
+		nodeSelectors: nodeSelectors,
+	}
+
+	startModelDeployInfo := &startJobInfo{
+		queue: queue,
+		//todo： 模型路径信息
+		modelPath: "undefined_model_path",
+		specs: startJobSpecs,
+	}
+
+	return startModelDeployInfo, nil
+}
+
+
+func (s *modelDeployService) getModelAndCheckPerm(ctx context.Context, userId string, spaceId string, modelId string) (*api.QueryModelReply, error) {
+    modelReq := &api.QueryModelRequest{}
+    modelReq.ModelId = modelId
+	reply, err := s.modelService.QueryModel(ctx, modelReq)
+	if err != nil {
+		return nil, err
+	}
+	if reply.Model == nil {
+		return nil, errors.Errorf(nil, errors.ErrorModelVersionFileNotFound)
+	}
+	//todo 待模型模块增加权限
+	if userId != reply.Model.UserId && reply.Model.IsPrefab != true  {
+		hasPerm := false
+		//for _, i := range reply.Model.Access {
+		//	if spaceId == i.SpaceId {
+		//		hasPerm = true
+		//		break
+		//	}
+		//}
+		if !hasPerm {
+			return nil, errors.Errorf(err, errors.ErrorModelNoPermission)
+		}
+	}
+
+	return reply, nil
+}
+
+//获取模型路径, todo 注意这里是否需要引入模型版本
+func (s *modelDeployService) getModelSubPath(model *model.ModelDeploy) string {
+	return fmt.Sprintf("%s/%s", common.GetMinioBucket(), common.GetMinioModelObject(model.WorkspaceId, model.UserId, model.ModelId, model.ModelVersion))
 }
 
 //停止模型服务
 func (s *modelDeployService) StopDepModel(ctx context.Context, req *api.StopDepRequest) (*api.StopDepReply, error) {
-	return nil, nil
+	_, err := s.data.ModelDeployDao.GetModelDeployService(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	//pipeline删除任务成功后，任务从running转为terminate转态会触发callback机制,更新base-server中的任务状态信息。
+	err = s.data.Pipeline.StopJob(ctx, &pipeline.UpdateJobParam{JobID: req.Id, Reason: req.Operation})
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now()
+	err = s.data.ModelDeployDao.UpdateModelDeployService(ctx, &model.ModelDeploy{
+		Id:          req.Id,
+		Operation:   req.Operation,
+		Status:      pipeline.STOPPED,
+		CompletedAt: &now,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.StopDepReply{StoppedAt: time.Now().Unix()}, nil
+
 }
 
 //删除模型服务
 func (s *modelDeployService) DeleteDepModel(ctx context.Context, req *api.DeleteDepRequest) (*api.DeleteDepReply, error) {
-	return nil, nil
+	jobs, _, err := s.data.ModelDeployDao.GetModelDeployServiceList(ctx, &model.ModelDeployListQuery{
+		UserId: req.UserId,
+		Ids:    req.JobIds,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, i := range jobs {
+		//只有任务是终止状态，才可以删除
+		if !pipeline.IsCompletedState(i.Status) {
+			return nil, errors.Errorf(nil, errors.ErrorDeleteJobRequest)
+		}
+
+		//train_job软删除
+		err = s.data.TrainJobDao.DeleteTrainJob(ctx, i.Id)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &api.DeleteDepReply{DeletedAt: time.Now().Unix()}, nil
 }
 
 //获取模型服务详情
