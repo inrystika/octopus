91f018a5fb20e37388293ad65b329042975ab0fc
wenlong92 <835952234@qq.com>
2022-02-14 17:42:56 +0800

    前端：#267：群组消费记录添加用户邮箱字段

--- admin-portal/src/views/timeManager/component/consumption.vue
+++ admin-portal/src/views/timeManager/component/consumption.vue
@@ -11,7 +11,13 @@
                     <span v-if="type=='group'">{{ scope.row.spaceName }}</span>
                 </template>
             </el-table-column>
-            <el-table-column prop="userName" label="用户名" v-if="type=='group'"> </el-table-column>
+            <el-table-column label="用户名" v-if="type=='group'">
+                <template slot-scope="scope">
+                    <el-tooltip trigger="hover" :content="scope.row.userEmail" placement="top">
+                        <span>{{ scope.row.userName }}</span>
+                    </el-tooltip>
+                </template>
+            </el-table-column>
             <el-table-column prop="title" label="任务名称"> </el-table-column>
             <el-table-column label="消费机时(h)" align="center">
                 <template slot-scope="scope">
