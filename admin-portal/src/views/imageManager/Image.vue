715b63067823999518220bb89b2ca70dd7ffb7b1
denglei <2522636547@qq.com>
2022-02-16 10:20:42 +0800

    搜索

--- admin-portal/src/views/imageManager/Image.vue
+++ admin-portal/src/views/imageManager/Image.vue
@@ -107,10 +107,6 @@
                     },
                     { type: 'Input', label: '镜像名', prop: 'imageNameLike', placeholder: '请输入镜像名' },
                     {
-                        type: 'Select', label: '镜像类型', prop: 'imageType', placeholder: '请选择镜像类型',
-                        options: [{ label: 'NoteBook类型', value: 1 }, { label: '训练类型', value: 2 }]
-                    },
-                    {
                         type: 'Select', label: '来源类型', prop: 'sourceType', placeholder: '请选择来源类型',
                         options: [{ label: '上传', value: 1 }, { label: '远程', value: 2 }]
                     }],
