796d7aad9845a1c8ac97d360e84c418910123863
denglei <2522636547@qq.com>
2022-02-24 11:26:12 +0800

    2-24bug修复

--- admin-portal/src/views/deployManager/modelDeployList.vue
+++ admin-portal/src/views/deployManager/modelDeployList.vue
@@ -83,7 +83,8 @@
                 detailDialog: false,
                 data: {},
                 statusText: { 'Preparing': ['status-ready', '初始中'], 'Creating': ['status-agent', '创建中'], 'Available': ['status-running', '运行中'], 'Failed': ['status-danger', '失败'], 'Stopped': ['status-stopping', '已停止'] },
-                searchForm: [
+                searchForm: [  { type: 'InputSelectUser', label: '用户', prop: 'userId', placeholder: '请输入用户名' },
+                    { type: 'InputSelectGroup', label: '群组', prop: 'workspaceId', placeholder: '请输入群组名' }
                 ],
                 searchData: {
                     pageIndex: 1,
