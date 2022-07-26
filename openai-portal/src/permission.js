acfcf4eead821f064d7afa7f1342a2588182ee7c
denglei <2522636547@qq.com>
2022-07-26 11:20:31 +0800

    token

--- openai-portal/src/permission.js
+++ openai-portal/src/permission.js
@@ -3,11 +3,12 @@ import store from './store'
 import { Message } from 'element-ui'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
-import { getToken } from '@/utils/auth' // get token from cookie
+import { getToken, setToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
+import { GetUrlParam } from '@/utils/index.js'
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
-const whiteList = ['/'] // no redirect whitelist
-router.beforeEach(async(to, from, next) => {
+const whiteList = ['/', '/register'] // no redirect whitelist
+router.beforeEach(async (to, from, next) => {
   // start progress bar
   NProgress.start()
 
@@ -17,6 +18,12 @@ router.beforeEach(async(to, from, next) => {
   // determine whether the user has logged in
   const hasToken = getToken()
   // next()
+  if (!hasToken) {
+    var url = window.location.href
+    if (url.indexOf('token') !== -1) {
+      setToken(GetUrlParam('token'))
+    }
+  }
   if (hasToken) {
     try {
       // eslint-disable-next-line eqeqeq
@@ -24,7 +31,7 @@ router.beforeEach(async(to, from, next) => {
       if (store.getters.workspaces.length === 0) { await store.dispatch('user/getSpace') }
     } catch (error) {
       await store.dispatch('user/resetToken')
-      Message.error(error || 'Has Error')
+      // Message.error('')
       next('/index')
       NProgress.done()
     }
@@ -32,15 +39,22 @@ router.beforeEach(async(to, from, next) => {
     if (to.path === '/') {
       next('/index')
       NProgress.done()
+    } else if (to.path === '/register') {
+      next('/index')
+      NProgress.done()
     } else {
       next()
     }
   } else {
-    /* has no token*/
-
     if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
-      next()
+      if (to.path === '/register') {
+        if (GetUrlParam('token') && GetUrlParam('token') !== '') {
+          setToken(GetUrlParam('token'))
+          next('/index')
+        } else {
+          next()
+        }
+      } else { next() }
     } else {
       // other pages that do not have permission to access are redirected to the login page.
       next(`/?redirect=${to.path}`)
@@ -48,7 +62,6 @@ router.beforeEach(async(to, from, next) => {
     }
   }
 })
-
 router.afterEach(() => {
   // finish progress bar
   NProgress.done()
