ead7ab7ebd695c04874b222fd417038859fe7fa9
denglei <2522636547@qq.com>
2022-10-13 12:49:47 +0800

    滚动条

--- admin-portal/src/directives.js
+++ admin-portal/src/directives.js
@@ -5,10 +5,7 @@ export default MyDirective.install = function(vue, options) {
         bind(el, binding) {
             const selectDom = el.querySelector('.el-select-dropdown .el-select-dropdown__wrap')
             selectDom.addEventListener('scroll', function() {
-                const isEnd = this.scrollHeight - this.scrollTop <= this.clientHeight
-                if (isEnd) {
                     binding.value()
-                }
             })
         }
     })
